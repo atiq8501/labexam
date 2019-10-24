cfcc172093fef7b02ec97600964f8562abda06ad
ATIQUR RAHMAN FOYSHAL <43756171+atiq8501@users.noreply.github.com>
2019-10-24 14:24:54 +0600

    fifth hour
    fifth hour

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var logout=require('./controllers/logout');
 var member=require('./controllers/member');
 var moderator=require('./controllers/moderator');
 var admin=require('./controllers/admin');
+var usermodel=require('./models/usermodel');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var multer = require('multer');
 var upload = multer({ dest: '/tmp/' });
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use('/login',login);
@@ -20,13 +20,23 @@ app.use('/member',member);
 app.use('/admin',admin);
 app.use('/moderator',moderator);
 app.use('/logout',logout);
-
-
-
-
 app.get('/',(req,res)=>{
-    res.render('home/home');
-})
+    var loggedinuser=req.cookies['username']
+    if(loggedinuser!= null){
+        usermodel.getByUsername(loggedinuser,(result)=>{
+            if(result.status==1)
+                res.redirect('/admin');
+            else if(result.status==2)
+                res.redirect('/moderator');
+            else if(result.status==3)
+                res.redirect('/member');
+        })
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+
 app.listen(port, ()=>{
     console.log('app is running in port: '+port.toString());
 });
